8f6c6dc927734e54dec15d6e494254d962750556
Sneha-14052003 <sneharawat1512@gmail.com>
2024-11-10 22:27:56 +0530

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    
+    inline void displaySum(int a, int b) {
+        cout << "Sum: " << (a + b) << endl;
+    }
+};
+
+int main() {
+    Student student;
+    student.displaySum(5, 10); 
+    return 0;
+}
